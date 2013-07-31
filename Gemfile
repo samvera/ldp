1bd0f2c0ad9f80ff25bb5cf28cb53b79a65e912c
Chris Beer <cabeer@stanford.edu>
2013-07-31 14:29:15 -0700

    remove byebugs from Gemfile

--- Gemfile
+++ Gemfile
@@ -2,5 +2,3 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in ldp-client.gemspec
 gemspec
-
-gem "byebug"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,8 +5,6 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'rspec/autorun'
 require 'ldp'
 require 'faraday'
-require "byebug" 
-
 RSpec.configure do |config|
 
 end
