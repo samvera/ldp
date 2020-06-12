fb668fc36ee61532c767ff270b2e9ab6947f07bd
Randall Floyd <rdfloyd@indiana.edu>
2020-06-12 13:12:52 -0400

    Satisfying style offenses after adding Bixby/Rubocop

--- lib/ldp/resource/rdf_source.rb
+++ lib/ldp/resource/rdf_source.rb
@@ -1,18 +1,17 @@
 require 'rdf/turtle'
 module Ldp
   class Resource::RdfSource < Ldp::Resource
-
     def initialize client, subject, graph_or_response = nil, base_path = ''
       super
 
       case graph_or_response
-        when RDF::Enumerable
-          @graph = graph_or_response
-        when Ldp::Response
-        when NilClass
-          #nop
-        else
-          raise ArgumentError, "Third argument to #{self.class}.new should be a RDF::Enumerable or a Ldp::Response. You provided #{graph_or_response.class}"
+      when RDF::Enumerable
+        @graph = graph_or_response
+      when Ldp::Response
+      when NilClass
+      # nop
+      else
+        raise ArgumentError, "Third argument to #{self.class}.new should be a RDF::Enumerable or a Ldp::Response. You provided #{graph_or_response.class}"
       end
     end
 
@@ -66,41 +65,42 @@ module Ldp
     end
 
     private
-      ##
-      # @param [Faraday::Response] graph query response
-      # @return [RDF::Graph]
-      def response_as_graph(resp)
-        graph = build_empty_graph
-        resp.each_statement do |stmt|
-          graph << stmt
+
+    ##
+    # @param [Faraday::Response] graph query response
+    # @return [RDF::Graph]
+    def response_as_graph(resp)
+      graph = build_empty_graph
+      resp.each_statement do |stmt|
+        graph << stmt
+      end
+      graph
+    end
+
+    ##
+    # @param [RDF::Graph] original_graph The graph returned by the LDP server
+    # @return [RDF::Graph] A graph stripped of any inlined resources present in the original
+    def filtered_graph(original_graph)
+      inlined_resources = original_graph.query(predicate: RDF::Vocab::LDP.contains).map { |x| x.object }
+
+      # we want to scope this graph to just statements about this model, not contained relations
+      if inlined_resources.empty?
+        original_graph
+      else
+        graph_without_inlined_resources(original_graph, inlined_resources)
+      end
+    end
+
+    def graph_without_inlined_resources(original_graph, inlined_resources)
+      new_graph = build_empty_graph
+
+      original_graph.each_statement do |s|
+        unless inlined_resources.include? s.subject
+          new_graph << s
         end
-        graph
       end
 
-      ##
-      # @param [RDF::Graph] original_graph The graph returned by the LDP server
-      # @return [RDF::Graph] A graph stripped of any inlined resources present in the original
-      def filtered_graph(original_graph)
-        inlined_resources = original_graph.query(predicate: RDF::Vocab::LDP.contains).map { |x| x.object }
-
-        # we want to scope this graph to just statements about this model, not contained relations
-        if inlined_resources.empty?
-          original_graph
-        else
-          graph_without_inlined_resources(original_graph, inlined_resources)
-        end
-      end
-
-      def graph_without_inlined_resources(original_graph, inlined_resources)
-        new_graph = build_empty_graph
-
-        original_graph.each_statement do |s|
-          unless inlined_resources.include? s.subject
-            new_graph << s
-          end
-        end
-
-        new_graph
-      end
+      new_graph
+    end
   end
 end
